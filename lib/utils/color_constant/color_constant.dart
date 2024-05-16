4ea861ee11cefb60ea2126e2f5a9f1d9e1cef464
AthiraaAthi <athiraa2612@gmail.com>
2024-05-16 14:18:08 +0530

    completed language_selection screen,starting splash screen

---
+++ lib/utils/color_constant/color_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static Color DefaultGrey = Color(0xffD9D9D9B2);
+  static Color black = Colors.black;
+}
--- lib/view/language_selection/language_selection.dart
+++ lib/view/language_selection/language_selection.dart
@@ -1,10 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
-class LanguageSelection extends StatelessWidget {
+class LanguageSelection extends StatefulWidget {
   const LanguageSelection({super.key});
 
   @override
+  State<LanguageSelection> createState() => _LanguageSelectionState();
+}
+
+class _LanguageSelectionState extends State<LanguageSelection> {
+  int _selectedValue = 1;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorConstant.DefaultGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: StatefulBuilder(
+                        builder: (context, StateSetter setState) {
+                          return Container(
+                            height: 350,
+                            width: 330,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Text(
+                                      'Select  Language',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Container(
+                                    height: 60,
+                                    width: 300,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.5),
+                                          spreadRadius: 1,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 5),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "Malayalam",
+                                              style: TextStyle(fontSize: 18.0),
+                                            ),
+                                          ),
+                                        ),
+                                        Radio<int>(
+                                          value: 1,
+                                          groupValue: _selectedValue,
+                                          onChanged: (int? newValue) {
+                                            setState(() {
+                                              _selectedValue = newValue!;
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 25,
+                                  ),
+                                  Container(
+                                    height: 60,
+                                    width: 300,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.5),
+                                          spreadRadius: 1,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 5),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              'English',
+                                              style: TextStyle(fontSize: 18.0),
+                                            ),
+                                          ),
+                                        ),
+                                        Radio<int>(
+                                          value: 2,
+                                          groupValue: _selectedValue,
+                                          onChanged: (int? newValue) {
+                                            setState(() {
+                                              _selectedValue = newValue!;
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 25),
+                                        child: Text(
+                                          "Next  >",
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500,
+                                            color: Color(
+                                              0xff07104E,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.circular(20)),
+                height: 60,
+                width: 250,
+                child: Center(
+                  child: Text(
+                    "Select your Language",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: ColorConstant.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
