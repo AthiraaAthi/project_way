9c13437c150dd9c027e923a1a7e11efca9b94ef1
AthiraaAthi <athiraa2612@gmail.com>
2024-05-16 12:17:53 +0530

    created screens

---
+++ lib/view/language_selection/language_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LanguageSelection extends StatelessWidget {
+  const LanguageSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
