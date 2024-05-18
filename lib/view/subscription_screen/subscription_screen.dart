94a5cb6e2d25a5a72f8eaa1038bcbd0a6e904e71
AthiraaAthi <athiraa2612@gmail.com>
2024-05-18 12:39:50 +0530

    completed subscription plans ui

--- lib/view/subscription_screen/subscription_screen.dart
+++ lib/view/subscription_screen/subscription_screen.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
+import 'package:project_way/view/home_screen/homescreen_widget.dart';
+import 'package:project_way/view/subscription_screen/subscription_widget.dart';
 
 class SubscriptionScreen extends StatelessWidget {
-  const SubscriptionScreen({super.key});
+  SubscriptionScreen({super.key});
+  final space = SizedBox(
+    height: 20,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Subscription Screen"),
-      ),
-    );
+        backgroundColor: ColorConstant.bgIndigo,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: ColorConstant.defIndigo,
+          title: Text(
+            "Subscription plans",
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SubscriptionWidget(
+                Title: "Bronze Package",
+                first: "Ai Chat(both text,voice )",
+                second: "Budget Handling without opening chat page",
+                third:
+                    "you can earn more referal income as 39.8 Rs and bonus income",
+                price: "199 Rs for 3 months",
+              ),
+              SubscriptionWidget(
+                Title: "Silver Package",
+                first: "Ai Chat(both text,voice )",
+                second: "Budget Handling without opening chat page",
+                third:
+                    "you can earn more referal income as 99.8 Rs and bonus income",
+                price: "499 Rs for 6 months",
+              ),
+              SubscriptionWidget(
+                Title: "Gold Package",
+                first: "Ai Chat(both text,voice )",
+                second: "Budget Handling without opening chat page",
+                third:
+                    "you can earn more referal income as 159.8 Rs and bonus income",
+                price: "699 Rs for 1 year",
+              ),
+              space
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/view/subscription_screen/subscription_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
+
+class SubscriptionWidget extends StatelessWidget {
+  const SubscriptionWidget(
+      {super.key,
+      required this.Title,
+      required this.first,
+      required this.second,
+      required this.third,
+      required this.price});
+  final String Title;
+  final String first;
+  final String second;
+  final String third;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+      child: Container(
+        height: 230,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+                spreadRadius: 1,
+                offset: Offset(0, 5))
+          ],
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5, left: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                Title,
+                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+              ),
+              Text(first),
+              Text(second),
+              Text(third),
+              Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: ColorConstant.defIndigo,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    price,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
