6b322ec2273ca74f64a3855a9b55bb5f9b39fdee
AthiraaAthi <athiraa2612@gmail.com>
2024-05-21 07:47:23 +0530

    started password screen

--- lib/view/passwords_screen/passwords_screen.dart
+++ lib/view/passwords_screen/passwords_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
 class PasswordsScreen extends StatelessWidget {
   const PasswordsScreen({super.key});
@@ -7,7 +9,27 @@ class PasswordsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Passwords Screen"),
+        backgroundColor: ColorConstant.defIndigo,
+        title: Text(
+          "My Passwords",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Text(
+            "Add New",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          )
+        ],
       ),
     );
   }
