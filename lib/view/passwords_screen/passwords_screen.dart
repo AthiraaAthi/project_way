2c0916e5022f9a8ae1662bc5fa4b051e2d320947
AthiraaAthi <athiraa2612@gmail.com>
2024-05-21 12:36:50 +0530

    COMPLETED PASSWORD screen

--- lib/view/passwords_screen/passwords_screen.dart
+++ lib/view/passwords_screen/passwords_screen.dart
@@ -180,6 +180,7 @@ class _PasswordsScreenState extends State<PasswordsScreen> {
                                       websiteController.clear();
                                       desController.clear();
                                       passController.clear();
+                                      setState(() {});
                                     },
                                     child: Container(
                                       height: 45,
@@ -227,69 +228,178 @@ class _PasswordsScreenState extends State<PasswordsScreen> {
           itemBuilder: (context, index) => Padding(
             padding: const EdgeInsets.all(8.0),
             child: PasswordWidget(
-                link: mylist[index]['website']!,
-                content: mylist[index]['description']!,
-                pass: mylist[index]['password']!,
-                onDeleteTap: () {
-                  setState(() {
-                    mylist.removeAt(index);
-                  });
-                },
-                onEditTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      websiteController.text = mylist[index]['website']!;
-                      desController.text = mylist[index]['description']!;
-                      passController.text = mylist[index]['password']!;
-                      return AlertDialog(
-                        title: Text("Edit Password"),
-                        content: SingleChildScrollView(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Container(
-                                child: TextField(
-                                  controller: websiteController,
-                                  decoration:
-                                      InputDecoration(labelText: "Website"),
-                                ),
-                              ),
-                              TextField(
-                                controller: desController,
-                                decoration:
-                                    InputDecoration(labelText: "Description"),
-                              ),
-                              TextField(
-                                controller: passController,
-                                decoration:
-                                    InputDecoration(labelText: "Password"),
-                              ),
-                            ],
+              link: mylist[index]['website']!,
+              content: mylist[index]['description']!,
+              pass: mylist[index]['password']!,
+              onDeleteTap: () {
+                ///////DELETE//////
+                setState(() {
+                  mylist.removeAt(index);
+                });
+              },
+              onEditTap: () {
+                /////////EDITING//////////
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    websiteController.text = mylist[index]['website']!;
+                    desController.text = mylist[index]['description']!;
+                    passController.text = mylist[index]['password']!;
+                    return Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Container(
+                          height: 580,
+                          width: 400,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                mylist[index]['website'] =
-                                    websiteController.text;
-                                mylist[index]['description'] =
-                                    desController.text;
-                                mylist[index]['password'] = passController.text;
-                                websiteController.clear();
-                                desController.clear();
-                                passController.clear();
-                                Navigator.pop(context);
-                              });
-                            },
-                            child: Text("Save"),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }),
+                          child: SingleChildScrollView(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Edit Password",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 13),
+                                  ),
+                                  SizedBox(
+                                    height: 25,
+                                  ),
+                                  Container(
+                                    height: 50,
+                                    width: 300,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: TextField(
+                                      controller: websiteController,
+                                      decoration: InputDecoration(
+                                        contentPadding: EdgeInsets.symmetric(
+                                            horizontal: 16.0, vertical: 12.0),
+                                        border: InputBorder.none,
+                                        hintText: "Website",
+                                        hintStyle: TextStyle(
+                                            fontSize: 12, color: Colors.grey),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Container(
+                                    height: 100,
+                                    width: 300,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: TextField(
+                                      controller: desController,
+                                      decoration: InputDecoration(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 16.0, vertical: 12.0),
+                                          border: InputBorder.none,
+                                          hintText: "Description",
+                                          hintStyle: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey)),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Container(
+                                    height: 50,
+                                    width: 300,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: TextField(
+                                      controller: passController,
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 16.0, vertical: 12.0),
+                                          border: InputBorder.none,
+                                          hintText: "Password",
+                                          hintStyle: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey)),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Text(
+                                    "you alrdy added your thumb impression for the password.if you want to change that,pls click below",
+                                    style: TextStyle(
+                                        color: ColorConstant.defGreen,
+                                        fontSize: 11.5,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    "click here to change thumb impression data",
+                                    style: TextStyle(
+                                        color: ColorConstant.defIndigo,
+                                        fontSize: 11.5,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 100,
+                                  ),
+                                  InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        mylist[index]['website'] =
+                                            websiteController.text;
+                                        mylist[index]['description'] =
+                                            desController.text;
+                                        mylist[index]['password'] =
+                                            passController.text;
+                                        websiteController.clear();
+                                        desController.clear();
+                                        passController.clear();
+                                        Navigator.pop(context);
+                                      });
+                                    },
+                                    child: Container(
+                                      height: 45,
+                                      width: 200,
+                                      decoration: BoxDecoration(
+                                          color: ColorConstant.defIndigo,
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: Center(
+                                        child: Text(
+                                          "Submit",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ),
       ),
