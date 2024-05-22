d6595ffd693e73eb70c07f2e481da472a0ddb085
AthiraaAthi <athiraa2612@gmail.com>
2024-05-22 09:49:06 +0530

    starting earnings screen

--- lib/view/earnings_screen/earnings_screen.dart
+++ lib/view/earnings_screen/earnings_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
 class EarningsScreen extends StatelessWidget {
   const EarningsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorConstant.bgBlue,
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
