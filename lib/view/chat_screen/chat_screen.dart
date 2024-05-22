5cea3dce4d40b819320cb4c768c4e0a1a567aa0a
AthiraaAthi <athiraa2612@gmail.com>
2024-05-22 11:14:15 +0530

    partially completed chat screen

--- lib/view/chat_screen/chat_screen.dart
+++ lib/view/chat_screen/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:project_way/utils/color_constant/color_constant.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,6 +9,7 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConstant.bgBlue,
       appBar: AppBar(
         backgroundColor: ColorConstant.defIndigo,
         leading: IconButton(
@@ -35,6 +38,72 @@ class ChatScreen extends StatelessWidget {
             ),
           )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                height: 30,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 214, 212, 212),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: Text(
+                    "1-5-2024",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 78,
+                  width: 185,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Color(0xff8bd3b3),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Can you add 15000 Rupees in income plan of today's date",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "11:10:am",
+                              style: TextStyle(fontSize: 10),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
