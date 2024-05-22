4e0518c933988d09acedaf40340708b546a05974
AthiraaAthi <athiraa2612@gmail.com>
2024-05-22 10:29:25 +0530

    started chat screen

--- lib/view/chat_screen/chat_screen.dart
+++ lib/view/chat_screen/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,7 +8,33 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chat Screen"),
+        backgroundColor: ColorConstant.defIndigo,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          "Way Ai",
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Text(
+              "Clear Chat",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w200),
+            ),
+          )
+        ],
       ),
     );
   }
