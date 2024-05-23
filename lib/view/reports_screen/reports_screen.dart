acdb80a9c4f24ffa885272871337c4543b69ac93
AthiraaAthi <athiraa2612@gmail.com>
2024-05-23 09:54:52 +0530

    partially completed reports screen

--- lib/view/reports_screen/reports_screen.dart
+++ lib/view/reports_screen/reports_screen.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:project_way/utils/color_constant/color_constant.dart';
 
-class ReportsScreen extends StatelessWidget {
+class ReportsScreen extends StatefulWidget {
   const ReportsScreen({super.key});
+
+  @override
+  State<ReportsScreen> createState() => _ReportsScreenState();
+}
+
+class _ReportsScreenState extends State<ReportsScreen> {
+  List<String> Numbers = [
+    "select year",
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+    "2024",
+    "2025",
+    "2026"
+  ];
+  String dropDownValue = "select year";
+  List<String> Months = [
+    "select month",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  String dropDownMonthValue = "select month";
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +59,87 @@ class ReportsScreen extends StatelessWidget {
             fontSize: 18,
             fontWeight: FontWeight.w600,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 50,
+                  width: 130,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: DropdownButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.black,
+                    ),
+                    underline: Container(),
+                    value: dropDownValue,
+                    items:
+                        Numbers.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            value,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        dropDownValue = value!;
+                      });
+                    },
+                  ),
+                ), //first drop down
+                Container(
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: DropdownButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.black,
+                    ),
+                    underline: Container(),
+                    value: dropDownMonthValue,
+                    items: Months.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            value,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        dropDownMonthValue = value!;
+                      });
+                    },
+                  ),
+                ), //second dropdown
+              ],
+            ),
+            Container()
+          ],
         ),
       ),
     );
