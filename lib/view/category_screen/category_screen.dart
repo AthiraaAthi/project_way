a06577d6218128a2475bcf904d3fe1df9e7bf614
AthiraaAthi <athiraa2612@gmail.com>
2024-05-24 09:58:26 +0530

    starting category screen

--- lib/view/category_screen/category_screen.dart
+++ lib/view/category_screen/category_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -6,8 +7,25 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConstant.bgBlue,
       appBar: AppBar(
+        backgroundColor: ColorConstant.defIndigo,
         title: Text("Category Screen"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Text(
+            "Add New",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
