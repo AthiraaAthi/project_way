db09f8fd9f13ef5d974ff4c7a06578336d574086
AthiraaAthi <athiraa2612@gmail.com>
2024-05-27 10:34:38 +0530

    started budget goal screen

--- lib/view/budget_goal/budget_goal_screen.dart
+++ lib/view/budget_goal/budget_goal_screen.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:project_way/utils/color_constant/color_constant.dart';
 
-class BudgetGoalScreen extends StatelessWidget {
+class BudgetGoalScreen extends StatefulWidget {
   const BudgetGoalScreen({super.key});
 
   @override
+  State<BudgetGoalScreen> createState() => _BudgetGoalScreenState();
+}
+
+class _BudgetGoalScreenState extends State<BudgetGoalScreen> {
+  List<String> numbers = [
+    "select year",
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+    "2024",
+    "2025",
+    "2026"
+  ];
+  String dropDownValue = "select year";
+  List<String> months = [
+    "select month",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConstant.bgBlue,
       appBar: AppBar(
-        title: Text("Budget Goal Screen"),
+        backgroundColor: ColorConstant.defIndigo,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          "Budget Goal",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [],
       ),
     );
   }
