52e1696fd1f43fc46864a73417dbb6103ebbb2f7
AthiraaAthi <athiraa2612@gmail.com>
2024-06-05 11:07:56 +0530

    modifying earnings widget

--- lib/view/earnings_screen/earnings_widget.dart
+++ lib/view/earnings_screen/earnings_widget.dart
@@ -22,75 +22,95 @@ class EarningsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 95,
-        width: 350,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 2),
-                  spreadRadius: 0.1,
-                  blurRadius: 1)
-            ]),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
+      child: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.width < 600 ? 95 : 150,
+          width: MediaQuery.of(context).size.width < 600 ? 350 : 700,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0, 2),
+                    spreadRadius: 0.1,
+                    blurRadius: 1)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height:
+                              MediaQuery.of(context).size.width < 600 ? 12 : 22,
+                          width:
+                              MediaQuery.of(context).size.width < 600 ? 12 : 22,
+                          color: color,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          title,
+                          style: MediaQuery.of(context).size.width < 600
+                              ? TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: ColorConstant.black,
+                                  fontSize: 13.7)
+                              : TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: ColorConstant.black,
+                                  fontSize: 25),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      dateTime,
+                      style: MediaQuery.of(context).size.width < 600
+                          ? TextStyle(fontSize: 12, fontWeight: FontWeight.w800)
+                          : TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w700),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Container(
-                        height: 12,
-                        width: 12,
-                        color: color,
-                      ),
-                      SizedBox(
-                        width: 10,
+                      Flexible(
+                        child: Text(
+                          subtitle,
+                          style: MediaQuery.of(context).size.width < 600
+                              ? TextStyle(fontSize: 12)
+                              : TextStyle(fontSize: 20),
+                        ),
                       ),
                       Text(
-                        title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: ColorConstant.black,
-                            fontSize: 13.7),
+                        amount,
+                        style: MediaQuery.of(context).size.width < 600
+                            ? TextStyle(
+                                color: amountColor,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              )
+                            : TextStyle(
+                                color: amountColor,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
                       ),
                     ],
                   ),
-                  Text(
-                    dateTime,
-                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w800),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        subtitle,
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: amountColor,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
