2b7be73e5d83a1cb4f3ba7460f5c19f6e0a92f9d
AthiraaAthi <athiraa2612@gmail.com>
2024-06-06 07:54:41 +0530

    modified container height

--- lib/view/notifications_screen/notifications_screen.dart
+++ lib/view/notifications_screen/notifications_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_way/utils/color_constant/color_constant.dart';
+import 'package:project_way/view/screen/responsive.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
@@ -8,96 +9,154 @@ class NotificationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: ColorConstant.bgBlue,
-        appBar: AppBar(
-          toolbarHeight: MediaQuery.of(context).size.width < 600 ? 60 : 80,
-          backgroundColor: ColorConstant.defIndigo,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: MediaQuery.of(context).size.width < 600
-                ? Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                  )
-                : Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                    size: 30,
-                  ),
+          backgroundColor: ColorConstant.bgBlue,
+          appBar: AppBar(
+            toolbarHeight: MediaQuery.of(context).size.width < 600 ? 60 : 80,
+            backgroundColor: ColorConstant.defIndigo,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: MediaQuery.of(context).size.width < 600
+                  ? Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    )
+                  : Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+            ),
+            title: Text(
+              "Notifications",
+              style: MediaQuery.of(context).size.width < 600
+                  ? TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600)
+                  : TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600),
+            ),
           ),
-          title: Text(
-            "Notifications",
-            style: MediaQuery.of(context).size.width < 600
-                ? TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600)
-                : TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(
-            top: 30,
-            left: 15,
-            right: 15,
-          ),
-          child: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 120,
-                width: 350,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 0.5,
-                          offset: Offset(0, 2),
-                          spreadRadius: 0.1)
-                    ]),
-                child: Padding(
+          body: ResponsiveWidget(
+            mobile: Padding(
+              padding: const EdgeInsets.only(
+                top: 30,
+                left: 15,
+                right: 15,
+              ),
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 7,
-                              backgroundColor: ColorConstant.defIndigo,
+                  child: Container(
+                    height: 120,
+                    width: 350,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 0.5,
+                              offset: Offset(0, 2),
+                              spreadRadius: 0.1)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 7,
+                                  backgroundColor: ColorConstant.defIndigo,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  "My Notifications title",
+                                  style: TextStyle(fontWeight: FontWeight.w900),
+                                )
+                              ],
                             ),
-                            SizedBox(
-                              width: 8,
-                            ),
-                            Text(
-                              "My Notifications title",
-                              style: TextStyle(fontWeight: FontWeight.w900),
-                            )
-                          ],
-                        ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+                            maxLines: 3,
+                            overflow: TextOverflow.visible,
+                          )
+                        ],
                       ),
-                      Text(
-                        "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
-                        maxLines: 3,
-                        overflow: TextOverflow.visible,
-                      )
-                    ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-      ),
+            //FOR TAB
+            tab: Padding(
+              padding: const EdgeInsets.only(
+                top: 30,
+                left: 15,
+                right: 15,
+              ),
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 220,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 0.5,
+                              offset: Offset(0, 2),
+                              spreadRadius: 0.1)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 7,
+                                  backgroundColor: ColorConstant.defIndigo,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  "My Notifications title",
+                                  style: TextStyle(fontWeight: FontWeight.w900),
+                                )
+                              ],
+                            ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+                            maxLines: 3,
+                            overflow: TextOverflow.visible,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
