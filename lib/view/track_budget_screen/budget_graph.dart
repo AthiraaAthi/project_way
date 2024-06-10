5f4a82fc2e538f39476eabca15fbb1c273eef181
AthiraaAthi <athiraa2612@gmail.com>
2024-06-10 08:31:56 +0530

    modified income flpoints

--- lib/view/track_budget_screen/budget_graph.dart
+++ lib/view/track_budget_screen/budget_graph.dart
@@ -30,23 +30,15 @@ class _BudgetGraphScreenState extends State<BudgetGraphScreen> {
   String _selectedMonth = 'Month';
 
   List<FlSpot> incomeData = [
+    FlSpot(0, 2), //
+    FlSpot(2, 500), //
+    FlSpot(500, 6),
     FlSpot(250, 1),
-    FlSpot(250, 2), FlSpot(250, 3),
-    // FlSpot(500, 7),
-    // FlSpot(1000, 3),
-    //FlSpot(1500, 5),
-    // FlSpot(2000, 9),
-    // FlSpot(2500, 1),
-    FlSpot(1500, 3),
-    FlSpot(1500, 4),
+    FlSpot(500, 7),
+    FlSpot(1000, 3),
     FlSpot(1500, 5),
-    FlSpot(1500, 6),
-    FlSpot(1500, 7),
-    FlSpot(1500, 8),
-    FlSpot(1500, 9),
-    FlSpot(1500, 10),
-    FlSpot(1500, 11),
-    FlSpot(1500, 12),
+    FlSpot(2000, 9),
+    FlSpot(2500, 1),
   ];
 
   List<FlSpot> expenseData = [
