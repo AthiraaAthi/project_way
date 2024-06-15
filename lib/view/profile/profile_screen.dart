71ccf2b564727e98e8f0bcbd9296ea5ea8320264
AthiraaAthi <athiraa2612@gmail.com>
2024-06-15 09:20:42 +0530

    adding one more container in profile screen for language selection

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -44,66 +44,67 @@ class ProfileScreen extends StatelessWidget {
             ),
           ),
           body: ResponsiveWidget(
-            mobile: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 300,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 0.5,
-                              offset: Offset(0, 2),
-                              spreadRadius: 0.1)
-                        ]),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        leading: Stack(
-                          alignment: Alignment.bottomRight,
-                          children: [
-                            Image.asset(ImageConstant.Profile),
-                            CircleAvatar(
-                              radius: 11,
-                              backgroundColor: ColorConstant.defIndigo,
-                              child: CircleAvatar(
+            mobile: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 0.5,
+                                offset: Offset(0, 2),
+                                spreadRadius: 0.1)
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: Stack(
+                            alignment: Alignment.bottomRight,
+                            children: [
+                              Image.asset(ImageConstant.Profile),
+                              CircleAvatar(
+                                radius: 11,
                                 backgroundColor: ColorConstant.defIndigo,
-                                radius: 5,
-                                child: Image.asset(
-                                  ImageConstant.cameraIcon,
-                                  fit: BoxFit.fitWidth,
+                                child: CircleAvatar(
+                                  backgroundColor: ColorConstant.defIndigo,
+                                  radius: 5,
+                                  child: Image.asset(
+                                    ImageConstant.cameraIcon,
+                                    fit: BoxFit.fitWidth,
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
-                        ),
-                        title: Text(
-                          "Tony George",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w900, fontSize: 15),
-                        ),
-                        subtitle: Text(
-                          "+91 974665155",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: 13,
-                              color: Colors.black),
+                              )
+                            ],
+                          ),
+                          title: Text(
+                            "Tony George",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900, fontSize: 15),
+                          ),
+                          subtitle: Text(
+                            "+91 974665155",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                fontSize: 13,
+                                color: Colors.black),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: 450,
-                    width: 300,
-                    decoration: BoxDecoration(
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 80,
+                      width: 300,
+                      decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
@@ -112,72 +113,107 @@ class ProfileScreen extends StatelessWidget {
                               blurRadius: 0.5,
                               offset: Offset(0, 2),
                               spreadRadius: 0.1)
-                        ]),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 200,
-                          child: Image.asset(ImageConstant.Trophy),
+                        ],
+                      ),
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 50,
+                        width: 280,
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.g_translate_rounded,
+                            color: ColorConstant.defIndigo,
+                            size: 30,
+                          ),
+                          title: Text(
+                            "English",
+                            style: TextStyle(),
+                          ),
                         ),
-                        Text(
-                          "₹ 1200.52",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: 20,
-                              color: Colors.black),
-                        ),
-                        Text(
-                          "Referal Count : 12",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20,
-                              color: Colors.black),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EarningsScreen(),
-                                ));
-                          },
-                          child: Container(
-                            height: 45,
-                            width: 250,
-                            decoration: BoxDecoration(
-                                color: ColorConstant.defIndigo,
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Center(
-                              child: Text(
-                                "View More",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 400,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 0.5,
+                                offset: Offset(0, 2),
+                                spreadRadius: 0.1)
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 200,
+                            child: Image.asset(ImageConstant.Trophy),
+                          ),
+                          Text(
+                            "₹ 1200.52",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            "Referal Count : 12",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EarningsScreen(),
+                                  ));
+                            },
+                            child: Container(
+                              height: 45,
+                              width: 250,
+                              decoration: BoxDecoration(
+                                  color: ColorConstant.defIndigo,
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Center(
+                                child: Text(
+                                  "View More",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Refer to earn",
-                          style: TextStyle(
-                              color: ColorConstant.defIndigo,
-                              fontWeight: FontWeight.w700),
-                        )
-                      ],
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Refer to earn",
+                            style: TextStyle(
+                                color: ColorConstant.defIndigo,
+                                fontWeight: FontWeight.w700),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 90,
-                  )
-                ],
+                    SizedBox(
+                      height: 90,
+                    )
+                  ],
+                ),
               ),
             ),
             //FOR TAB
