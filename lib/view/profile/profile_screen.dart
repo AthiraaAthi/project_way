5e607f32a77a14392820a30a638a77537ea27006
AthiraaAthi <athiraa2612@gmail.com>
2024-06-19 10:11:07 +0530

    added language selection dialog box same as the beginning screen in the profile screen

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:project_way/utils/color_constant/color_constant.dart';
 import 'package:project_way/utils/image_constant/image_constant.dart';
 import 'package:project_way/view/earnings_screen/earnings_screen.dart';
+import 'package:project_way/view/login_screen/login_screen.dart';
 import 'package:project_way/view/screen/responsive.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,6 +15,338 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   int _selectedValue = 1;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _showDialog();
+    });
+    super.initState();
+  }
+
+  void _showDialog() {
+    MediaQuery.of(context).size.width < 600
+        ? showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: StatefulBuilder(
+                  builder: (context, StateSetter setState) {
+                    return Container(
+                      height: 350,
+                      width: 330,
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: Text(
+                                'Select  Language',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w800),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Container(
+                              height: 60,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: ColorConstant.DefaultGrey,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 0.1,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 5),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "Malayalam",
+                                        style: TextStyle(fontSize: 18.0),
+                                      ),
+                                    ),
+                                  ),
+                                  Radio<int>(
+                                    value: 1,
+                                    groupValue: _selectedValue,
+                                    onChanged: (int? newValue) {
+                                      setState(() {
+                                        _selectedValue = newValue!;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Container(
+                              height: 60,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: ColorConstant.DefaultGrey,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 0.1,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 5),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'English',
+                                        style: TextStyle(fontSize: 18.0),
+                                      ),
+                                    ),
+                                  ),
+                                  Radio<int>(
+                                    value: 2,
+                                    groupValue: _selectedValue,
+                                    onChanged: (int? newValue) {
+                                      setState(() {
+                                        _selectedValue = newValue!;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 25),
+                                  child: InkWell(
+                                    onTap: () {
+                                      Locale selectedLocale =
+                                          _selectedValue == 1
+                                              ? Locale('ml')
+                                              : Locale('en');
+                                      context.setLocale(selectedLocale);
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => LoginScreen(),
+                                          ));
+                                    },
+                                    child: Text(
+                                      "Next  >",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        color: Color(
+                                          0xff07104E,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          )
+        : showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: StatefulBuilder(
+                  builder: (context, StateSetter setState) {
+                    return Container(
+                      height: 500,
+                      width: 530,
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: Text(
+                                'Select  Language',
+                                style: TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.w800),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Container(
+                              height: 80,
+                              width: 450,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: ColorConstant.DefaultGrey,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 0.2,
+                                    blurRadius: 0.5,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "Malayalam",
+                                        style: TextStyle(fontSize: 23),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Transform.scale(
+                                      scale: 2,
+                                      child: Radio<int>(
+                                        materialTapTargetSize:
+                                            MaterialTapTargetSize.shrinkWrap,
+                                        value: 1,
+                                        groupValue: _selectedValue,
+                                        onChanged: (int? newValue) {
+                                          setState(() {
+                                            _selectedValue = newValue!;
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Container(
+                              height: 80,
+                              width: 450,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: ColorConstant.DefaultGrey,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    spreadRadius: 0.2,
+                                    blurRadius: 0.5,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'English',
+                                        style: TextStyle(fontSize: 23),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Transform.scale(
+                                      scale: 2,
+                                      child: Radio<int>(
+                                        value: 2,
+                                        groupValue: _selectedValue,
+                                        onChanged: (int? newValue) {
+                                          setState(() {
+                                            _selectedValue = newValue!;
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 70,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 25),
+                                  child: InkWell(
+                                    onTap: () {
+                                      Locale selectedLocale =
+                                          _selectedValue == 1
+                                              ? Locale('ml')
+                                              : Locale('en');
+                                      context.setLocale(selectedLocale);
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => LoginScreen(),
+                                          ));
+                                    },
+                                    child: Text(
+                                      "Next  >",
+                                      style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w500,
+                                        color: Color(
+                                          0xff07104E,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
