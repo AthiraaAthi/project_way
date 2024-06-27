a16b2323b438877b07f28dacf174210f6d8771aa
AthiraaAthi <athiraa2612@gmail.com>
2024-06-27 09:20:49 +0530

    added json to a  tile of notification screen

--- lib/view/notifications_screen/notifications_screen.dart
+++ lib/view/notifications_screen/notifications_screen.dart
@@ -82,14 +82,14 @@ class NotificationScreen extends StatelessWidget {
                                   width: 8,
                                 ),
                                 Text(
-                                  "My Notifications title",
+                                  "notifications.notificationTitle".tr(),
                                   style: TextStyle(fontWeight: FontWeight.w900),
                                 )
                               ],
                             ),
                           ),
                           Text(
-                            "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+                            "notifications.notificationBody".tr(),
                             maxLines: 3,
                             overflow: TextOverflow.visible,
                           )
