190ebf64e9807a825873e9e2cd3a0e62324605f4
AthiraaAthi <athiraa2612@gmail.com>
2024-06-29 17:32:16 +0530

    completed coming soon screen

--- lib/view/coming_soon_screen/comingsoon_screen.dart
+++ lib/view/coming_soon_screen/comingsoon_screen.dart
@@ -10,51 +10,53 @@ class ComingSoonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ColorConstant.bgIndigo,
-        body: ResponsiveWidget(
-          mobile: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(ImageConstant.ComingSoon),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  "ComingSoonScreen.ComingSoon".tr(),
-                  style: TextStyle(
-                      color: ColorConstant.defIndigo,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 35),
-                ),
-              ],
-            ),
+      backgroundColor: ColorConstant.bgIndigo,
+      body: ResponsiveWidget(
+        mobile: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(ImageConstant.ComingSoon),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "ComingSoonScreen.ComingSoon".tr(),
+                style: TextStyle(
+                    color: ColorConstant.defIndigo,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 35),
+              ),
+            ],
           ),
-          //FOR TAB
-          tab: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    height: 200,
-                    width: 200,
-                    child: Image.asset(
-                      ImageConstant.ComingSoon,
-                      fit: BoxFit.cover,
-                    )),
-                SizedBox(
-                  height: 30,
+        ),
+        //FOR TAB
+        tab: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                    ImageConstant.ComingSoon,
+                    fit: BoxFit.cover,
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "ComingSoonScreen.ComingSoon".tr(),
+                style: TextStyle(
+                  color: ColorConstant.defIndigo,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 55,
                 ),
-                Text(
-                  "Coming Soon",
-                  style: TextStyle(
-                      color: ColorConstant.defIndigo,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 55),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
